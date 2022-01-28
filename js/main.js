a7e678bcbe39aac7b99804edf5e703c73e034493
Vasya <96485073+Vasya050@users.noreply.github.com>
2022-01-29 00:52:01 +0700

    Добавил файлы
    Добавил модальное окно, сделал фон

--- js/main.js
+++ js/main.js
@@ -12,3 +12,17 @@ window.onload = function () {
 	jQuery("#user-city").text(ymaps.geolocation.city);
 	jQuery("#user-city2").text(ymaps.geolocation.city);
 }
+
+const modalWindow = document.querySelector('.modal');
+const buttonModal = document.querySelector('.main-display__button');
+
+buttonModal.addEventListener('click', () => {
+    modalWindow.classList.add('active');
+});
+
+modalWindow.addEventListener('click', (e) => {
+    const isModal = e.target.closest('.modal__inner');
+    if (!isModal) {
+        modalWindow.classList.remove('active');
+    }
+});
